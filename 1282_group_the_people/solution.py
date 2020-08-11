86bf64a508bc08c9c445f7780d5b4b8bd6d32193
Louis-C-Leon <louistheleon@gmail.com>
2020-08-11 15:00:11 -0700

    Group the people problem

---
+++ 1282_group_the_people/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    
+    
+    def groupThePeople(self, groupSizes):
+        """
+        :type groupSizes: List[int]
+        :rtype: List[List[int]]
+        """
+        output = []
+        groups = {}
+        for i in range(len(groupSizes)):
+            size = groupSizes[i]
+            if size in groups:
+                groups[size].append(i)
+            else:
+                groups[size] = [i]
+                
+        for size in groups:
+            idx = 0;
+            group = groups[size]
+            while(len(group) > 0):
+                subgroup = group[idx:idx + size]
+                group = group[idx + size:]
+                output.append(subgroup)
+        
+        return output
