2fe8b9c3b7079587e380d6cd7c600ed699c262ff
Louis-C-Leon <louistheleon@gmail.com>
2020-10-30 12:18:09 -0700

    Sliding window solution to longest unique substring

--- 3_longest_substring_without_repeating_characters/solution.py
+++ 3_longest_substring_without_repeating_characters/solution.py
@@ -1,21 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         length = 0
-        i = 0
-        while i < len(s):
-            dict = {}
-            curr_len = 0
-            unique = True
-            while unique == True and i < len(s):
-                char = s[i]
-                if char in dict:
-                    unique = False
-                    i = dict[char] + 1
-                else:
-                    curr_len += 1
-                    dict[char] = i
-                    i += 1
-            if curr_len > length:
-                length = curr_len
-        return length
+        start = 0
+        end = 0
+        chars = set()
+        while end < len(s):
+            if s[end] in chars:
+                chars.remove(s[start])
+                start += 1
+            else:
+                chars.add(s[end])
+                end += 1
+                length = max(length, end - start)
 
+        return length
